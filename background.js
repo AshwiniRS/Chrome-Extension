be126bf95e8e86d9d0e8def4e420857e42db397f
AshwiniRS <ashwinisarode4@gmail.com>
2017-01-10 09:10:59 -0500

    added other context menus

--- background.js
+++ background.js
@@ -1,11 +1,26 @@
 //available only in background environment
-chrome.contextMenus.create({
-    title:"Twitter Social Toolkit",
-    contexts:["selection"],
-    onclick:myfunction 
-});
 
-function myfunction(selectedText){
-    //alert(selectedText.selectionText);
-    chrome.tabs.create({url:"https://twitter.com/intent/tweet?text="+selectedText.selectionText});
+var contextsList = ["selection","link","image","page"];
+for(i=0;i<contextsList.length;i++){
+    var context = contextsList[i];
+    var titleX = "Twitter toolkit: Share this "+context+" on your twitter profile";
+    chrome.contextMenus.create({title:titleX, contexts:[context], onclick:myfunction,id:context});
+}
+
+
+function myfunction(data){
+    switch(data.menuItemId){
+        case 'selection' : 
+            chrome.tabs.create({url:"https://twitter.com/intent/tweet?text="+data.selectionText});
+            break;
+        case 'link' :
+            chrome.tabs.create({url:"https://twitter.com/intent/tweet?text="+data.linkUrl});
+            break;
+        case 'image' :
+            chrome.tabs.create({url:"https://twitter.com/intent/tweet?text="+data.srcUrl});
+            break;
+        case 'page' :
+            chrome.tabs.create({url:"https://twitter.com/intent/tweet?text=MyPage"});
+            break;
+    }
 }
