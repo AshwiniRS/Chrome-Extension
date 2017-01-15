132b76ae1cfadafe0e13d0a7eec7c04823fd0348
AshwiniRS <ashwinisarode4@gmail.com>
2017-01-15 16:48:58 -0500

    my extension initial commit

---
+++ Personalize/background.js
@@ -0,0 +1,28 @@
+////available only in background environment
+//
+//var contextsList = ["selection","link","image","page"];
+//for(i=0;i<contextsList.length;i++){
+//    var context = contextsList[i];
+//    var titleX = "Twitter toolkit: Share this "+context+" on your twitter profile";
+//    chrome.contextMenus.create({title:titleX, contexts:[context], onclick:clickHandler,id:context});
+//}
+//
+//
+//function clickHandler(data,tab){
+//    switch(data.menuItemId){
+//        case 'selection' : 
+//            chrome.windows.create({url:"https://twitter.com/intent/tweet?text="+encodeURIComponent(data.selectionText),type:"panel"});
+//            break;
+//        case 'link' :
+//            chrome.windows.create({url:"https://twitter.com/intent/tweet?text="+encodeURIComponent(data.linkUrl),type:"panel"});
+//            break;
+//        case 'image' :
+//            chrome.windows.create({url:"https://twitter.com/intent/tweet?text="+encodeURIComponent(data.srcUrl),type:"panel"});
+//            break;
+//        case 'page' :
+//            chrome.windows.create({url:"https://twitter.com/intent/tweet?text="+encodeURIComponent(tab.title),type:"panel"});
+//            break;
+//    }
+//}
+
+alert("hi");
---
+++ Personalize/script.js
@@ -0,0 +1,35 @@
+$(function(){
+
+//quotes 
+$.get('quotes.txt', function(data) {
+   var quotes = data.split("\n");
+   var randLineNum = Math.floor(Math.random() * (quotes.length-1));
+    alert(randLineNum);
+   //alert(quotes[randLineNum]);
+//***************** To iterate on file **********************    
+    
+//     $.each(quote, function(n, elem) {
+//       $('#myContainer').append('<div>' + elem + '</div>');
+//    });
+
+// *********************************************************
+     $('#quoteContainer').append('<div>' + quotes[randLineNum] + '</div>');
+    
+});
+
+//focus of the day
+$('#focus').bind("enterKey",function(e){
+   //do stuff here
+    var value = $('#focus').val();
+    var div = "<div id='labelFocus' class='labelDiv' text-align= 'center'>" + value+ "</div>";
+    $(this).replaceWith(div);
+    console.log(value);
+});
+$('#focus').keyup(function(e){
+    if(e.keyCode == 13)
+    {
+        $(this).trigger("enterKey");
+    }
+});
+
+});
